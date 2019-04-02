c2b91128a2ac34e13487803f191042449542a627
Student <example@example.com>
2019-04-02 16:15:14 -0700

    follow weekend homework

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
+  }
 
-  getAlbums() {
-    return ALBUMS;
+  getAlbums(){
+    return this.albums;
+  }
+
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
+  getAlbumById(albumId: string){
+    return this.database.object('albums/' + albumId);
+  }
+
+  removeAlbum(removeAlbum: Album) {
+    delete removeAlbum.description;
+    delete removeAlbum.title;
+    delete removeAlbum.artist;
   }
 
 }
